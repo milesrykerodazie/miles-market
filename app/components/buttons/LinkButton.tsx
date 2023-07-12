fb58396620dcb64a0dae418d6fc1bcb12a9506c7
Miles Ryker Odazie <milesodazie@gmail.com>
2023-07-12 01:49:23 +0100

    Clean Up on link

--- app/components/buttons/LinkButton.tsx
+++ app/components/buttons/LinkButton.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 const LinkButton = () => {
    return (
       <Link
-         href='/admin-dashboard/category/new'
+         href='/admin-dashboard/category'
          className='px-4 py-1 text-center text-white bg-primary dark:bg-white dark:text-primary font-semibold text-sm md:text-base border border-transparent rounded-md trans my-3'
       >
          Add Category
